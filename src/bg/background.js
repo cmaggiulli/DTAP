9249bac48dc437a7999616c3d3bacbfcaf36ff2b
Chris Maggiulli <cmaggiulli@gmail.com>
2017-06-01 11:02:43 -0400

    First Commit

---
+++ src/bg/background.js
@@ -0,0 +1,7 @@
+// message handler inject script
+chrome.extension.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		chrome.pageAction.show(sender.tab.id);
+		sendResponse();
+	}
+);
